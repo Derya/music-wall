18c8062a1458154042dca05fda3d7774e7ea6251
Derya Aydede <derya.aydede@gmail.com>
2016-07-11 12:05:52 -0700

    tracks page

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,9 @@
 class Track < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 127 }
   validates :author, presence: true, length: { maximum: 127 }
-  validates :song_url, format: { with: /@^(https?|ftp)://[^\s/$.?#].[^\s]*$@iS/, message: "must be a url" }, allow_nil: true
-  validates :picture_url, format: { with: /@^(https?|ftp)://[^\s/$.?#].[^\s]*$@iS/, message: "must be a url" }, allow_nil: true
+  validates :song_url, format: { with: /[a-z]/, message: "must be a url" }, allow_nil: true
+  validates :picture_url, format: { with: /[a-z]/, message: "must be a url" }, allow_nil: true
   
 end
+
+#@^(https?|ftp)://[^\s/$.?#].[^\s]*$@iS
