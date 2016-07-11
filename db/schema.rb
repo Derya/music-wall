4e11eab9809497b3e77a236ab577a8387c8cc076
Derya Aydede <derya.aydede@gmail.com>
2016-07-11 11:53:10 -0700

    built db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160711185002) do
+
+  create_table "tracks", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "song_url"
+    t.string   "picture_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
